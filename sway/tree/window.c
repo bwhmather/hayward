aca06c643346c39f9b768767ddf6a1b3d7a0bcfa
Ben Mather <bwhmather@bwhmather.com>
2022-05-22 22:26:43 +0100

    Remove duplicate docstrings

--- sway/tree/window.c
+++ sway/tree/window.c
@@ -400,32 +400,12 @@ static void window_move_to_column_from_maybe_direction(
 	}
 }
 
-/**
- * Detaches a window from its current column, and by extension workspace, and
- * attaches it to a new column, possibly in a different workspace.  Where in
- * the new column it is inserted is determined by the direction of movement.
- *
- * If the window was previously floating or fullscreen, all of that state will
- * be cleared.
- *
- * The window will not be automatically focused.
- */
 void window_move_to_column_from_direction(
 		struct sway_container *win, struct sway_container *col,
 		enum wlr_direction move_dir) {
 	window_move_to_column_from_maybe_direction(win, col, true, move_dir);
 }
 
-/**
- * Detaches a window from its current column, and by extension workspace, and
- * attaches it to a new column, possibly in a different workspace.  The window
- * will be move immediately after the column's last focused child.
- *
- * If the window was previously floating or fullscreen, all of that state will
- * be cleared.
- *
- * The window will not be automatically focused.
- */
 void window_move_to_column(struct sway_container *win,
 		struct sway_container *col) {
 	window_move_to_column_from_maybe_direction(win, col, false, WLR_DIRECTION_DOWN);
@@ -493,15 +473,6 @@ static void window_move_to_workspace_from_maybe_direction(
 	}
 }
 
-/**
- * Detaches a window from its current workspace and column, and attaches it to
- * a different one.  The new column, and location within that column, are
- * determined by the direction of movement.
- *
- * If the window is floating or fullscreen, it will remain so.
- *
- * The window will not be automatically focused.
- */
 void window_move_to_workspace_from_direction(
 		struct sway_container *win, struct sway_workspace *ws,
 		enum wlr_direction move_dir) {
