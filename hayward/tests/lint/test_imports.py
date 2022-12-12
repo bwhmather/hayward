bdf71a63c42c5f1f6a737cfc5dadbce1db922319
Ben Mather <bwhmather@bwhmather.com>
2022-12-12 20:48:04 +0000

    Extract out some of the core logic for resolving paths and parsing

--- hayward/tests/lint/test_imports.py
+++ hayward/tests/lint/test_imports.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import os
 import pathlib
@@ -6,58 +7,106 @@ import unittest
 
 import clang.cindex
 
-SOURCE_ROOT = pathlib.Path(os.environ["MESON_SOURCE_ROOT"])
-BUILD_ROOT = pathlib.Path(os.environ["MESON_BUILD_ROOT"])
+SOURCE_ROOT = pathlib.Path(os.environ["MESON_SOURCE_ROOT"]).resolve()
+BUILD_ROOT = pathlib.Path(os.environ["MESON_BUILD_ROOT"]).resolve()
+
+HAYWARD_ROOT = SOURCE_ROOT / pathlib.Path("hayward")
+HAYWARD_INCLUDE_ROOT = SOURCE_ROOT / pathlib.Path("include/hayward")
+
 
 _raw_commands = json.loads((BUILD_ROOT / "compile_commands.json").read_text())
-cpp_args = {}
+CPP_ARGS = {}
 for _command in _raw_commands:
-    _path = (pathlib.Path(_command["directory"]) / _command["file"]).resolve()
-    cpp_args[_path] = [
+    _path = pathlib.Path(_command["directory"]) / _command["file"]
+    _path = _path.resolve()
+    CPP_ARGS[_path] = [
         arg
         for arg in shlex.split(_command["command"])
         if arg.startswith(("-I", "-D", "-std"))
     ]
 
 
+INDEX = clang.cindex.Index.create()
+
+
+def enumerate_source_files():
+    files = [
+        source_file.relative_to(SOURCE_ROOT)
+        for source_file in HAYWARD_ROOT.glob("**/*.c")
+    ]
+    yield from sorted(files)
+
+
+def resolve_header_path(source_path, /):
+    path = (
+        HAYWARD_INCLUDE_ROOT
+        / (SOURCE_ROOT / source_path).relative_to(HAYWARD_ROOT).parent
+        / (source_path.stem + ".h")
+    ).relative_to(SOURCE_ROOT)
+
+    if not (SOURCE_ROOT / path).exists():
+        return None
+
+    return path
+
+
+def resolve_source_path(header_path, /):
+    path = (
+        HAYWARD_ROOT
+        / (SOURCE_ROOT / header_path).relative_to(HAYWARD_INCLUDE_ROOT)
+        / (header_path.stem + ".c")
+    ).relative_to(SOURCE_ROOT)
+
+    if not path.exists():
+        return None
+
+    return path
+
+
+@functools.lru_cache(maxsize=None)
+def parse_header_file(header_path):
+    source_path = resolve_source_path(header_path)
+    if source_path is None:
+        source_path = pathlib.Path("hayward/server.c")
+
+    args = CPP_ARGS[SOURCE_ROOT / source_path]
+    return INDEX.parse(SOURCE_ROOT / header_path, args=args)
+
+
+@functools.lru_cache(maxsize=None)
+def parse_source_file(source_path):
+    args = CPP_ARGS[SOURCE_ROOT / source_path]
+    return INDEX.parse(SOURCE_ROOT / source_path, args=args)
+
+
 class TestSourceMatchesHeader(unittest.TestCase):
-    def test_source_matches_header(self):
-        source_dir = SOURCE_ROOT / pathlib.Path("hayward")
-        include_dir = SOURCE_ROOT / pathlib.Path("include/hayward")
+    def test_source_matches_header_order(self):
+        for source_path in enumerate_source_files():
+            with self.subTest(file=source_path):
+                header_path = resolve_header_path(source_path)
+                if header_path is None:
+                    self.skipTest(f"No header for {source_path}")
 
-        for source_file in source_dir.glob("**/*.c"):
-            source_file = source_file.resolve()
-            include_file = (
-                include_dir
-                / source_file.parent.relative_to(source_dir)
-                / (source_file.stem + ".h")
-            ).resolve()
-            if not include_file.exists():
-            	# TODO most source files should have a corresponding header.
-            	continue
+                header = parse_header_file(header_path)
+                source = parse_source_file(source_path)
 
-            with self.subTest(file=source_file.name):
-                index = clang.cindex.Index.create()
-                hdr = index.parse(include_file, args=cpp_args[source_file])
-                src = index.parse(source_file, args=cpp_args[source_file])
-
-                hdr_decls = [
+                header_decls = [
                     node.spelling
-                    for node in hdr.cursor.get_children()
+                    for node in header.cursor.get_children()
                     if node.kind == clang.cindex.CursorKind.FUNCTION_DECL
-                    and node.location.file.name == hdr.spelling
+                    and node.location.file.name == header.spelling
                 ]
-                src_defs = [
+                source_defs = [
                     node.spelling
-                    for node in src.cursor.get_children()
+                    for node in source.cursor.get_children()
                     if node.kind == clang.cindex.CursorKind.FUNCTION_DECL
                     and node.is_definition()
                     and node.storage_class != clang.cindex.StorageClass.STATIC
-                    and node.location.file.name == src.spelling
+                    and node.location.file.name == source.spelling
                 ]
-                self.assertEqual(set(hdr_decls), set(src_defs))
-                self.assertEqual(hdr_decls, src_defs)
+                self.assertEqual(set(header_decls), set(source_defs))
+                self.assertEqual(header_decls, source_defs)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
