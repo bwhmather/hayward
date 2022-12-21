34511c1ed47b8b23750ccbae569454df877bc19f
Ben Mather <bwhmather@bwhmather.com>
2022-12-21 23:48:48 +0000

    Use fully resolved paths for headers and source files

--- hayward/tests/lint/test_imports.py
+++ hayward/tests/lint/test_imports.py
@@ -31,65 +31,101 @@ for _command in _raw_commands:
 INDEX = clang.cindex.Index.create()
 
 
-def enumerate_source_files():
-    files = [
-        source_file.relative_to(SOURCE_ROOT)
-        for source_file in HAYWARD_ROOT.glob("**/*.c")
-    ]
-    yield from sorted(files)
+def enumerate_source_paths():
+    paths = [source_path for source_path in HAYWARD_ROOT.glob("**/*.c")]
+    yield from sorted(paths)
 
 
-def enumerate_header_files():
-    files = [
-        source_file.relative_to(SOURCE_ROOT)
-        for source_file in HAYWARD_INCLUDE_ROOT.glob("**/*.h")
-    ]
-    yield from sorted(files)
+def enumerate_header_paths():
+    paths = [header_path for header_path in HAYWARD_INCLUDE_ROOT.glob("**/*.h")]
+    yield from sorted(paths)
+
+
+def is_source_path(path, /):
+    assert path.is_absolute()
+
+    return path.suffix == ".c"
+
+
+def is_header_path(path, /):
+    assert path.is_absolute()
+
+    return path.suffix == ".h"
+
+
+def is_hayward_source_path(path, /):
+    assert path.is_absolute()
+
+    if not is_source_path(path):
+        return False
+
+    if not path.is_relative_to(HAYWARD_ROOT):
+        return False
+
+    return True
+
+
+def is_hayward_header_path(path, /):
+    assert path.is_absolute()
+
+    if not is_header_path(path):
+        return False
+
+    if not path.is_relative_to(HAYWARD_INCLUDE_ROOT):
+        return False
+
+    return True
 
 
 def header_path_for_source_path(source_path, /):
-    path = (
-        HAYWARD_INCLUDE_ROOT
-        / (SOURCE_ROOT / source_path).relative_to(HAYWARD_ROOT).parent
-        / (source_path.stem + ".h")
-    ).relative_to(SOURCE_ROOT)
+    assert is_source_path(source_path)
+    assert source_path.is_relative_to(HAYWARD_ROOT)
 
-    if not (SOURCE_ROOT / path).exists():
+    header_path = (
+        HAYWARD_INCLUDE_ROOT
+        / source_path.relative_to(HAYWARD_ROOT).parent
+        / (source_path.stem + ".h")
+    )
+
+    if not header_path.exists():
         return None
 
-    return path
+    return header_path
 
 
 def source_path_for_header_path(header_path, /):
-    path = (
+    assert is_header_path(header_path)
+    assert header_path.is_relative_to(HAYWARD_INCLUDE_ROOT)
+
+    source_path = (
         HAYWARD_ROOT
-        / (SOURCE_ROOT / header_path).relative_to(HAYWARD_INCLUDE_ROOT)
+        / header_path.relative_to(HAYWARD_INCLUDE_ROOT).parent
         / (header_path.stem + ".c")
     ).relative_to(SOURCE_ROOT)
 
-    if not path.exists():
+    if not source_path.exists():
         return None
 
-    return path
+    return source_path
+
+
+def get_args(path, /):
+    if is_header_path(path):
+        source_path = source_path_for_header_path(path)
+        if source_path is None:
+            source_path = HAYWARD_ROOT / "server.c"
+        args = CPP_ARGS[source_path]
+    else:
+        args = CPP_ARGS[path]
+    return args
 
 
 @functools.lru_cache(maxsize=None)
-def parse_header_file(header_path):
-    source_path = source_path_for_header_path(header_path)
-    if source_path is None:
-        source_path = pathlib.Path("hayward/src/server.c")
-
-    args = CPP_ARGS[SOURCE_ROOT / source_path]
-    return INDEX.parse(SOURCE_ROOT / header_path, args=args)
+def parse_path(path, /):
+    return INDEX.parse(path, args=get_args(path))
 
 
-@functools.lru_cache(maxsize=None)
-def parse_source_file(source_path):
-    args = CPP_ARGS[SOURCE_ROOT / source_path]
-    return INDEX.parse(SOURCE_ROOT / source_path, args=args)
-
-
-def normalize_clang_path(path):
+def resolve_clang_path(path):
     path = pathlib.Path(path)
     if not path.is_absolute():
         path = BUILD_ROOT / path
@@ -97,11 +133,42 @@ def normalize_clang_path(path):
     return path
 
 
+@functools.lru_cache(maxsize=None)
+def include_dirs(source_path):
+    include_dirs = [source_path.parent]
+
+    include_dirs += [
+        BUILD_ROOT / pathlib.Path(arg[2:])
+        for arg in get_args(source_path)
+        if arg.startswith("-I")
+    ]
+
+    return include_dirs
+
+
+@functools.lru_cache(maxsize=None)
+def resolve_include_path(source_path, include_path, /):
+    assert source_path.is_absolute()
+    assert isinstance(include_path, str)
+
+    for candidate_dir in include_dirs(source_path):
+        candidate_path = candidate_dir / include_path
+        candidate_path = candidate_path.resolve()
+        if candidate_path.exists():
+            return candidate_path
+
+    raise Exception(f"Could not resolve {include_path}")
+
+
+def same_clang_path(a, b):
+    return resolve_clang_path(a) == resolve_clang_path(b)
+
+
 class DeclarationOrderTestCase(unittest.TestCase):
     maxDiff = 2000
 
     def test_source_and_header_orders_match(self):
-        for source_path in enumerate_source_files():
+        for source_path in enumerate_source_paths():
             with self.subTest(file=source_path):
                 if source_path.match("hayward/src/commands.c"):
                     self.skipTest("Command modules share a single header")
@@ -113,15 +180,15 @@ class DeclarationOrderTestCase(unittest.TestCase):
                 if header_path is None:
                     self.skipTest(f"No header for {source_path}")
 
-                header = parse_header_file(header_path)
-                source = parse_source_file(source_path)
+                header = parse_path(header_path)
+                source = parse_path(source_path)
 
                 header_decls = [
                     node.spelling
                     for node in header.cursor.get_children()
                     if node.kind == clang.cindex.CursorKind.FUNCTION_DECL
-                    and normalize_clang_path(node.location.file.name)
-                    == normalize_clang_path(header.spelling)
+                    and resolve_clang_path(node.location.file.name)
+                    == resolve_clang_path(header.spelling)
                 ]
                 source_defs = [
                     node.spelling
@@ -129,8 +196,8 @@ class DeclarationOrderTestCase(unittest.TestCase):
                     if node.kind == clang.cindex.CursorKind.FUNCTION_DECL
                     and node.is_definition()
                     and node.storage_class != clang.cindex.StorageClass.STATIC
-                    and normalize_clang_path(node.location.file.name)
-                    == normalize_clang_path(source.spelling)
+                    and resolve_clang_path(node.location.file.name)
+                    == resolve_clang_path(source.spelling)
                 ]
 
                 self.assertEqual(set(header_decls), set(source_defs))
@@ -139,14 +206,15 @@ class DeclarationOrderTestCase(unittest.TestCase):
     def test_tree_header_orders_match(self):
         header_paths = [
             header_path
-            for header_path in enumerate_header_files()
-            if header_path.is_relative_to("hayward/include/hayward/tree")
+            for header_path in enumerate_header_paths()
+            if header_path.is_relative_to(HAYWARD_INCLUDE_ROOT / "tree")
         ]
+        assert header_paths
 
         decls = {}
 
         for header_path in header_paths:
-            header = parse_header_file(header_path)
+            header = parse_path(header_path)
             prefix = header_path.stem
 
             header_decls = [
@@ -179,34 +247,35 @@ class DeclarationOrderTestCase(unittest.TestCase):
                 self.assertEqual(header_a_decls, header_b_decls)
 
     def test_commands_and_headers_match(self):
-        source_paths = [pathlib.Path("hayward/src/commands.c")]
+        source_paths = [HAYWARD_ROOT / "commands.c"]
         source_paths += [
             source_path
-            for source_path in enumerate_source_files()
-            if source_path.is_relative_to("hayward/src/commands")
+            for source_path in enumerate_source_paths()
+            if source_path.is_relative_to(HAYWARD_ROOT / "commands")
         ]
+        assert len(source_paths) > 1
 
         source_defs = []
         for source_path in source_paths:
-            source = parse_source_file(source_path)
+            source = parse_path(source_path)
             source_defs += [
                 node.spelling
                 for node in source.cursor.get_children()
                 if node.kind == clang.cindex.CursorKind.FUNCTION_DECL
                 and node.is_definition()
                 and node.storage_class != clang.cindex.StorageClass.STATIC
-                and normalize_clang_path(node.location.file.name)
-                == normalize_clang_path(source.spelling)
+                and resolve_clang_path(node.location.file.name)
+                == resolve_clang_path(source.spelling)
             ]
 
-        header_path = pathlib.Path("hayward/include/hayward/commands.h")
-        header = parse_header_file(header_path)
+        header_path = HAYWARD_INCLUDE_ROOT / "commands.h"
+        header = parse_path(header_path)
         header_decls = [
             node.spelling
             for node in header.cursor.get_children()
             if node.kind == clang.cindex.CursorKind.FUNCTION_DECL
-            and normalize_clang_path(node.location.file.name)
-            == normalize_clang_path(header.spelling)
+            and resolve_clang_path(node.location.file.name)
+            == resolve_clang_path(header.spelling)
         ]
 
         self.assertEqual(set(header_decls), set(source_defs))
@@ -218,11 +287,12 @@ class IncludeTestCase(unittest.TestCase):
 
         # Build dependency graph.
         graph = {}
-        for header in enumerate_header_files():
-            name = str(header.relative_to("hayward/include"))
-            path = SOURCE_ROOT / header
-            source = path.read_text()
-            graph[name] = {match.group(1) for match in pattern.finditer(source)}
+        for header_path in enumerate_header_paths():
+            source = header_path.read_text()
+            graph[header_path] = {
+                resolve_include_path(header_path, match.group(1))
+                for match in pattern.finditer(source)
+            }
 
         # Check for cycles.
         visited = set()
