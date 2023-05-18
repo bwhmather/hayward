356bf09de74b2832851c5d4ca34d3004261d5b13
Ben Mather <bwhmather@bwhmather.com>
2023-05-18 22:13:42 +0100

    Add test to make sure include guards are correct

---
+++ hayward/tests/lint/test_include_guards.py
@@ -0,0 +1,54 @@
+"""
+Checks that all source and header files directly import the headers that they
+need.
+"""
+import itertools
+import sys
+import re
+import difflib
+
+from hayward_lint import (
+    INCLUDE_ROOT,
+    PROJECT_ROOT,
+    enumerate_header_paths,
+)
+
+
+def test():
+    passed = True
+
+    for header_path in itertools.chain(enumerate_header_paths()):
+        with header_path.open() as header:
+            actual_guard = [
+                header.readline(),
+                header.readline(),
+                header.readline(),
+            ]
+
+        guard_name = "HAYWARD_" + re.sub(
+            "[-/.]", "_", str(header_path.relative_to(INCLUDE_ROOT)).upper()
+        )
+
+        expected_guard = [
+            f"#ifndef {guard_name}\n",
+            f"#define {guard_name}\n",
+            "\n",
+        ]
+        if actual_guard != expected_guard:
+            msg = "======================================================================\n"
+            msg += f"FAIL: test_include_guards: {header_path.relative_to(PROJECT_ROOT)}\n"
+            msg += "----------------------------------------------------------------------\n"
+            msg += "Header include guards do not match expected:\n"
+
+            for diff_line in difflib.ndiff(expected_guard, actual_guard):
+                msg += f"  {diff_line}"
+            msg += "\n"
+
+            print(msg, file=sys.stderr)
+            passed = False
+
+    return passed
+
+
+if __name__ == "__main__":
+    sys.exit(0 if test() else 1)
